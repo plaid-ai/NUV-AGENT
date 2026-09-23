af4d3044f1a5e527063a4f6a4cf923f1b8286cc4
jihoseo <swiftsjh@plaid.ai.kr>
2026-09-23 13:27:28 +0900

    release: prepare 0.1.135

--- packaging/homebrew/nuv-agent.rb
+++ packaging/homebrew/nuv-agent.rb
@@ -5,7 +5,7 @@ class NuvAgent < Formula
   homepage "https://github.com/plaid-ai/NUV-agent"
   url "__URL__"
   sha256 "__SHA256__"
-  version "0.1.134"
+  version "0.1.135"
   license "Proprietary"
 
   depends_on :macos
